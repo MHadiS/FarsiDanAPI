0f7238d188fc59e24ee0d2d923a70beed8e37432
MHadiS <hadishakib6561@gmail.com>
2022-11-17 11:47:25 +0330

    FEAT: create question model

--- question/models.py
+++ question/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Questions(models.Model):
+    text = models.TextField()
+    option_1 = models.TextField()
+    option_2 = models.TextField()
+    option_3 = models.TextField()
+    option_4 = models.TextField()
+    correct_option = models.CharField(max_length=8)
+    question_type = models.CharField(max_length=10)  # historical, dictation, grammar, vocabulary and books
+    difficultly = models.CharField(max_length=6)  # easy, normal and hard
+    from_tizhooshan_exam = models.BooleanField()
