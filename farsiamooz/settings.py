55a6cd0a3fa66f00be6dc3f2b5f4650047ba51cc
MHadiS <hadishakib6561@gmail.com>
2022-11-17 11:17:39 +0330

    FEAT: create question app

--- farsiamooz/settings.py
+++ farsiamooz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'question.apps.QuestionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
