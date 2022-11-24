73dc39ad4e59b9f6c7e83e3be9253323f921555e
MHadiS <hadishakib6561@gmail.com>
2022-11-25 00:49:43 +0330

    FIX: fix the bug about missing accepted column when creating a new record

--- question/serializers.py
+++ question/serializers.py
@@ -8,3 +8,9 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Questions
         fields = ("text", "option_1", "option_2", "option_3", "option_4", "correct_option",
                   "question_type", "difficulty", "from_tizhooshan_exam", "grade", "chapter_no")
+
+    def create(self, validated_data):
+        return Questions.objects.create(
+            accepted=False,
+            **validated_data
+        )
