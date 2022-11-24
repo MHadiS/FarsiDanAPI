64fbdcfc563dda27ecd1b820638cbcea36908965
MHadiS <hadishakib6561@gmail.com>
2022-11-25 01:27:16 +0330

    FIX: use function for some validators instead of lambda

--- question/models.py
+++ question/models.py
@@ -7,6 +7,16 @@ def categorise_validator(value, categories):
         raise ValidationError(f"'question_type' just can be {', '.join(categories)}")
 
 
+def question_type_validator(value):
+    categorise = ("historical", "diction", "grammar", "vocabulary", "books")
+    categorise_validator(value, categorise)
+
+
+def difficulty_validator(value):
+    categorise = ("easy", "normal", "hard")
+    categorise_validator(value, categorise)
+
+
 class Questions(models.Model):
     text = models.TextField(unique=True)
     option_1 = models.TextField(unique=True)
@@ -16,16 +26,10 @@ class Questions(models.Model):
     correct_option = models.IntegerField(validators=[MaxValueValidator(4),
                                                      MinValueValidator(1)])
     question_type = models.CharField(max_length=10, validators=[
-        lambda value: categorise_validator(
-            value,
-            ("historical", "diction", "grammar", "vocabulary", "books")
-        )
-    ])  # question just type can be : historical, dictation, grammar, vocabulary and books
+        question_type_validator
+    ])  # historical, dictation, grammar, vocabulary and books"""
     difficulty = models.CharField(max_length=6, validators=[
-        lambda value: categorise_validator(
-            value,
-            ("easy", "normal", "hard")
-        )
+        difficulty_validator
     ])  # easy, normal and hard
     from_tizhooshan_exam = models.BooleanField()
     grade = models.IntegerField(validators=[MinValueValidator(1),
