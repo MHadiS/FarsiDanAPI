e472f80f0dc4276ae53637eeaf0dd462b8e92d5e
MHadiS <hadishakib6561@gmail.com>
2022-12-22 16:25:38 +0330

    FEAT: add id to QuestionSerializer fields

--- question/serializers.py
+++ question/serializers.py
@@ -6,7 +6,7 @@ from .models import Questions
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Questions
-        fields = ("title", "text", "option_1", "option_2", "option_3", "option_4", "correct_option",
+        fields = ("id", "title", "text", "option_1", "option_2", "option_3", "option_4", "correct_option",
                   "question_type", "difficulty", "from_tizhooshan_exam", "grade", "chapter_no")
 
     def create(self, validated_data):
