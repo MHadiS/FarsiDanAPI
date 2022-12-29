64eb7f9b759e81fad63bac69dddbd7ee64009543
MHadiS <hadishakib6561@gmail.com>
2022-12-29 15:08:31 +0330

    FEAT: update the admin panel

--- question/admin.py
+++ question/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Questions
 
 # Register your models here.
-admin.site.register(Questions)
+@admin.register(Questions)
+class AminQuestions(admin.ModelAdmin):
+    list_display = ("title", "question_type", "difficulty", "chapter_no", "from_tizhooshan_exam", "grade", "accepted")
+    search_fields = ("title", "text")
+    list_filter = ("question_type", "difficulty", "chapter_no", "from_tizhooshan_exam", "grade", "accepted")
