9d1f512da250be7d80ceb920e94bdf17284aa528
MHadiS <hadishakib6561@gmail.com>
2022-12-31 23:54:23 +0330

    REFACTOR: change the config.json location

--- farsiamooz/settings.py
+++ farsiamooz/settings.py
@@ -15,7 +15,7 @@ import os
 import json
 
 
-with open('/etc/config.json') as config_file:
+with open('./config.json') as config_file:
     config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
