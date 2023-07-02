eebfc021320e381fee0a4d27fe80ac809fc2bb62
MHadiS <hadishakib6561@gmail.com>
2023-07-02 11:42:56 +0000

    REFACTOR: use config.json

--- farsiamooz/settings.py
+++ farsiamooz/settings.py
@@ -18,6 +18,10 @@ import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read some data from config.json file
+with open(BASE_DIR / 'config.json') as config_file:
+        config = json.load(config_file)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -78,13 +82,7 @@ WSGI_APPLICATION = 'farsiamooz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = config["DATABASES"]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
