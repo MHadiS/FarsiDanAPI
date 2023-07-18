1c980f0a82833e4b7c3caa858b2e61055f2c5d9d
MHadiS <hadishakib6561@gmail.com>
2023-07-18 12:34:23 +0330

    REFACTOR: refine the syllabus app models

--- syllabus/models.py
+++ syllabus/models.py
@@ -1,10 +1,29 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+
+from question.models import RegisteredBook
 
 
-class Syllabuses(models.Model):
-    name = models.CharField(max_length=50)  # the file path
-    chapter_no = models.PositiveIntegerField(unique=True)
-    grade = models.PositiveIntegerField(
-        validators=[MaxValueValidator(12)]
-    )  # 0 < grade < 13
+class Tag(models.Model):
+    class Meta:
+        verbose_name = "تگ"
+        verbose_name_plural = "تگ ها"
+
+    name = models.CharField(max_length=10, unique=True, verbose_name="نام")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Syllabus(models.Model):
+    class Meta:
+        verbose_name = "جزوه"
+        verbose_name_plural = "جزوه ها"
+    
+    title = models.CharField(max_length=20, verbose_name="عنوان")
+    book = models.ForeignKey(RegisteredBook, on_delete=models.CASCADE, verbose_name="کتاب")
+    tags = models.ManyToManyField(Tag, verbose_name="تگ ها")
+    pdf_file = models.FileField(upload_to="syllabuses", verbose_name="فایل pdf")
+    banner = models.ImageField(default="banners/default.jpg", upload_to="banners", verbose_name="بنر")
+
+    def __str__(self) -> str:
+        return f"جزوه : {self.title}"
