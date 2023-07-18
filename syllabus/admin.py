ff49d0968739d85ce015320d00c854c637a8ebfb
MHadiS <hadishakib6561@gmail.com>
2023-07-18 12:35:39 +0330

    FEAT: register models in admin panel

--- syllabus/admin.py
+++ syllabus/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import Syllabuses
+from .models import Syllabus, Tag
 
 
 # Register some models
-# admin.site.register(Syllabuses)
+@admin.register(Syllabus)
+class SyllabusAdmin(admin.ModelAdmin):
+    list_display = ("title", "book")
+    search_fields = ("title",)
+    list_filter = ["book", "tags"]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
--- syllabus/apps.py
+++ syllabus/apps.py
@@ -5,3 +5,4 @@ from django.apps import AppConfig
 class SyllabusConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "syllabus"
+    verbose_name = "مدیریت جزوه ها"
