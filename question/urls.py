cff02e3646cb8def6ad2d16b2d3cf7a184b3766d
MHadiS <hadishakib6561@gmail.com>
2023-07-18 12:31:05 +0330

    REFACTOR: rename some urls

--- question/urls.py
+++ question/urls.py
@@ -3,7 +3,7 @@ from .views import get_questions, get_attributes
 
 
 urlpatterns = [
-    path("", get_questions, name="questions"),  # get the questions
-    path("attributes/<str:category>", get_attributes, name="attributes"),  # get some data about filters of questions
-    path("attributes/<str:category>/<int:data_id>", get_attributes, name="attributes_by_id"),  # get some data about filters of questions
+    path("", get_questions, name="get-questions"),  # get the questions
+    path("attributes/<str:category>", get_attributes, name="get-attributes"),  # get some data about filters of questions
+    path("attributes/<str:category>/<int:data_id>", get_attributes, name="get-attributes-by-id"),  # get some data about filters of questions
 ]
